78cd7c32d82d02421605a0b8e11cf1811c9cc606
Saebryn <kwanner4mt@gmail.com>
2022-01-23 12:18:58 -0800

    more DOM stuff

--- javascript-practice/DOM/js-file.js
+++ javascript-practice/DOM/js-file.js
@@ -37,10 +37,50 @@ container2P.textContent = "ME TOO!";
 container2.appendChild(container2P);
 
 const btn2 = document.querySelector('#btn2');
-btn2.onclick = () => alert("Hello World");
+btn2.onclick = () => alert("Hello World2");
 
-//best method
+/* //best method
 const btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click', () => {
-  alert("Hello World");
+  alert("Hello World3");
+}); */
+
+// can also be done like so. when calling the function to run, 
+//leave out the parenthases. these cause the function to run 
+//automatically at page load.
+function alertFunction() {
+  alert ("Yay! Hello World4");
+}
+
+btn3.addEventListener('click', alertFunction);
+
+//here you can use e.(property) to pull specific info
+btn3.addEventListener('click', function (e) {
+  console.log(e);
 });
+
+//altering the style of the event target (button 3 here)
+btn3.addEventListener('click', function (e) {
+  e.target.style.backgroundColor = 'blue';
+});
+
+//node lists. selects all button selectors:
+const buttons = document.querySelectorAll('button');
+
+//use the forEach method to iterate through all elements
+//of the node list
+buttons.forEach((button) => {
+  //display the button's id on click
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
+
+//lots of events:
+/* 
+click
+dblclick
+keypress
+keydown
+keyup
+ */
